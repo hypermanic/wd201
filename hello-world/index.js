b1040c2cff7078d13dda2faa141c3a97427e2ecf
hypermanic <forbuzzvaishnav@gmail.com>
2023-12-01 16:45:57 +0530

    re build of repo

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,6 +1,10 @@
-const hello = () => {
-    console.log("Hello Node.js!");
-  };
-  
-  hello();
-  
+const http = require("http");
+const fs = requireed("fs");
+
+const server = http.createServer((res,res)=>{
+  /*fs.readFile("sample.txt",(err,data)=>{
+    res.end(data);*/
+    const stream = fs.createReadStream("sample.txt");
+  })
+})
+server.listen(3000);
